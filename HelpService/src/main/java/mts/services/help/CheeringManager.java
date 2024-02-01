d6d0d7e68e69b619478c168b269abfe74e1f681c
Mark <101831381+LilacShark@users.noreply.github.com>
2024-02-02 02:21:57 +0700

    Простой сервлет сделан. Томкат пока отдельно стоит. Коммит перед написанием тестов

--- HelpService/src/main/java/mts/services/help/CheeringManager.java
+++ HelpService/src/main/java/mts/services/help/CheeringManager.java
@@ -1,4 +1,20 @@
 package mts.services.help;
 
 public class CheeringManager {
+
+    private final CheeringInMemRepository repository;
+
+    public CheeringManager() {
+        this.repository = new CheeringInMemRepository();
+    }
+
+    public String getCheeringPhrase() {
+
+        return repository.getCheeringPhrase();
+    }
+
+    public String addCheeringPhrase(String phrase) {
+        repository.addCheeringPhrase(phrase);
+        return "Добалвена фраза: " + phrase;
+    }
 }
--- HelpService/src/main/java/mts/services/help/CheeringServlet.java
+++ HelpService/src/main/java/mts/services/help/CheeringServlet.java
@@ -1,5 +1,6 @@
 package mts.services.help;
 
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,23 +10,32 @@ import java.io.IOException;
 
 public class CheeringServlet extends HttpServlet {
 
+    private CheeringManager manager;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        this.manager = new CheeringManager();
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        resp.setContentType("text/html");
+        resp.getWriter().println("<html><body>");
+        resp.getWriter().println(manager.getCheeringPhrase());
+        resp.getWriter().println("</body></html>");
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        resp.setContentType("text/html");
+        resp.getWriter().println("<html><body>");
+        resp.getWriter().println(manager.addCheeringPhrase(req.getParameter("phrase")));
+        resp.getWriter().println("</body></html>");
     }
 
-    @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPut(req, resp);
+
+    public CheeringServlet() {
     }
 
-    @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doDelete(req, resp);
-    }
 }
